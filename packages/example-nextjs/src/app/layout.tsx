ba65f49c21ae189990d15b9b419b22509a7d0bbb
asmyshlyaev177 <asmyshlyaev177@gmail.com>
2024-07-08 13:55:17 +0400

    chore: vercel analytics

--- packages/example-nextjs/src/app/layout.tsx
+++ packages/example-nextjs/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
   );
 }
