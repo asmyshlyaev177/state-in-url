31f337ac68598b00f7cc8234e65e3ba01c7269fa
asmyshlyaev177 <asmyshlyaev177+github@gmail.com>
2024-07-17 13:02:24 +0400

    style: roboto font

--- packages/example-nextjs/tailwind.config.ts
+++ packages/example-nextjs/tailwind.config.ts
@@ -7,7 +7,13 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  // theme: { colors: { ...colors } },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-roboto)'],
+      },
+    },
+  },
   plugins: [],
 };
 export default config;
