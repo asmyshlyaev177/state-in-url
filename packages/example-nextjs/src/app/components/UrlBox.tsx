7baaab4b1816d3bd17e3ccb2a5a90e40213b46a0
asmyshlyaev177 <asmyshlyaev177@gmail.com>
2024-08-05 16:55:25 +0400

    style: url box and some style fixes

---
+++ packages/example-nextjs/src/app/components/UrlBox.tsx
@@ -0,0 +1,23 @@
+'use client';
+import { useSearchParams } from 'next/navigation';
+import React from 'react';
+
+export const UrlBox = () => {
+  const sp = useSearchParams();
+  const [url, setUrl] = React.useState('');
+
+  React.useEffect(() => {
+    setUrl(
+      window.location.pathname + window.location.search + window.location.hash,
+    );
+  }, [sp]);
+
+  return (
+    <input
+      id="url"
+      readOnly
+      className="w-full bg-slate-100 rounded-sm text-gray-700 font-mono"
+      value={`url=${url}`}
+    />
+  );
+};
