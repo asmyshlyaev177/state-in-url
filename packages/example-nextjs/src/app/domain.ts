e1176aae24d696a3337e71403e67b19323dbdf41
asmyshlyaev177 <asmyshlyaev177+github@gmail.com>
2024-08-12 20:51:01 +0400

    chore: clean up

--- packages/example-nextjs/src/app/domain.ts
+++ packages/example-nextjs/src/app/domain.ts
@@ -1,8 +1,6 @@
 export const isVercel = !!process.env.VERCEL;
 
 export const vercelUrl = 'https://state-in-url-asmyshlyaev177.vercel.app';
-export const netifyUrl = 'https://state-in-url.netlify.app/';
+export const netifyUrl = 'https://state-in-url.netlify.app';
 
 export const siteUrl = isVercel ? vercelUrl : netifyUrl;
-
-console.log({ isVercel, siteUrl });
