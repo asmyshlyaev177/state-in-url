e648c403c7a6ef4527262aa8e885e791aa59a5a1
asmyshlyaev177 <asmyshlyaev177@gmail.com>
2024-08-12 11:19:05 +0400

    style: shiki highlighter

--- packages/example-nextjs/src/app/components/File.tsx
+++ packages/example-nextjs/src/app/components/File.tsx
@@ -1,21 +1,34 @@
-import SyntaxHighlighter from 'react-syntax-highlighter';
-import { a11yDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import {
+  transformerNotationHighlight,
+  transformerNotationWordHighlight,
+} from '@shikijs/transformers';
+import { codeToHtml } from 'shiki';
 
-export const File = ({ name, content }: { name: string; content: string }) => {
+export const File = async ({
+  name,
+  content,
+}: {
+  name: string;
+  content: string;
+}) => {
+  const html = await codeToHtml(content, {
+    lang: 'typescript',
+    theme: 'github-dark',
+    transformers: [
+      transformerNotationHighlight(),
+      transformerNotationWordHighlight(),
+    ],
+  });
+
   return (
     <noindex>
       <div
-        className="shadow-md  bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700 p-4"
+        className="codeblock shadow-md bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700 p-4"
         data-nosnippet="true"
       >
         <div className="font-mono text-sm pb-2 select-none">{name}</div>
-        <SyntaxHighlighter
-          language="typescript"
-          style={a11yDark}
-          wrapLongLines={true}
-        >
-          {content}
-        </SyntaxHighlighter>
+
+        <div dangerouslySetInnerHTML={{ __html: html }}></div>
       </div>
     </noindex>
   );
