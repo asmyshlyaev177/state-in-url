617d7e67f54401fe87dd967e6ef4dead18ded7ca
asmyshlyaev177 <asmyshlyaev177@gmail.com>
2024-08-15 11:59:59 +0400

    test: extended jsdom test env

--- jest.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-export default {
-  testRegex: '(\\.|/)(test)\\.[jt]sx?$',
-  preset: 'ts-jest',
-  // testEnvironment: 'node',
-  testEnvironment: 'jsdom'
-};
