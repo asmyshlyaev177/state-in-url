4ec5a9b6f7383bf61d86dd0f506ec53373c14890
asmyshlyaev177 <asmyshlyaev177+github@gmail.com>
2024-08-20 16:12:00 +0400

    chore: noopener for links

--- packages/example-nextjs/src/app/page.tsx
+++ packages/example-nextjs/src/app/page.tsx
@@ -67,10 +67,18 @@ export default async function Home({ searchParams }: { searchParams: object }) {
       </div>
 
       <footer className="text-gray-700 text-right mt-4 flex flex-col">
-        <a href="https://github.com/asmyshlyaev177" target="_blank">
+        <a
+          href="https://github.com/asmyshlyaev177"
+          target="_blank"
+          rel="noopener"
+        >
           © asmyshlyaev177 {new Date().getFullYear()}
         </a>
-        <a href="https://www.npmjs.com/package/state-in-url" target="_blank">
+        <a
+          href="https://www.npmjs.com/package/state-in-url"
+          target="_blank"
+          rel="noopener"
+        >
           state in url npm package
         </a>
       </footer>
