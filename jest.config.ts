2354557c73fa5b2ce2cd3df815081eb47e6703bb
asmyshlyaev177 <asmyshlyaev177@gmail.com>
2024-08-24 21:14:08 +0400

    ci: codacy coverage

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,9 @@ const config: Config ={
   testRegex: '(\\.|/)(test)\\.[jt]sx?$',
   preset: 'ts-jest',
   testEnvironment: './test.env.ts',
+  "collectCoverage": true,
+  "coverageReporters": ["json", "html", "lcov"],
+  coverageDirectory: './coverage-reports'
 };
 
 export default config
