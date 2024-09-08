01cb2132b8f0314d3f261e518b3dd246fb2e35eb
asmyshlyaev177 <asmyshlyaev177@gmail.com>
2024-09-08 10:43:51 +0400

    chore: remove analytic

--- packages/example-nextjs/next-env.d.ts
+++ packages/example-nextjs/next-env.d.ts
@@ -3,9 +3,3 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
-
-declare module '*.css' {
-  const content: { [className: string]: string };
-  export = content;
-}
-export type {};
