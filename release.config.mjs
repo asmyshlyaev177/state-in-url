da2bb7322c7078de9f23538c372b89f0d6465255
asmyshlyaev177 <asmyshlyaev177+github@gmail.com>
2024-09-08 21:27:01 +0400

    ci: release-it

--- release.config.mjs
+++
@@ -1,8 +0,0 @@
-/**
- * @type {import('semantic-release').GlobalConfig}
- */
-export default {
-  // branches: ["main", "next"],
-  repositoryUrl: "git@github.com:asmyshlyaev177/state-in-url.git",
-  plugins: ['@semantic-release/git', '@semantic-release/commit-analyzer', '@semantic-release/exec', '@semantic-release/release-notes-generator', '@semantic-release/npm', '@semantic-release/github']
-};
