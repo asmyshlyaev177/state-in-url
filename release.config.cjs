659e6050d89dd38cb6cfa952d5c1d746e69d7c56
asmyshlyaev177 <asmyshlyaev177@gmail.com>
2024-09-09 10:37:02 +0400

    fix: semantic-release config

---
+++ release.config.cjs
@@ -0,0 +1,22 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "CHANGELOG.md"
+      }
+    ],
+    [
+      "@semantic-release/git",
+      {
+        "assets": ["CHANGELOG.md"]
+      }
+    ]
+  ]
+};
