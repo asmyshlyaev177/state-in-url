e07340c7dfd3a59b159501ba205e050616f89c00
asmyshlyaev177 <asmyshlyaev177@gmail.com>
2024-10-01 17:01:36 +0400

    fix: fix ts types

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,32 @@
 import resolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 import sourcemaps from 'rollup-plugin-sourcemaps';
+import typescript from '@rollup/plugin-typescript';
 import filesize from 'rollup-plugin-filesize';
 import { glob } from 'glob'
-import sucrase from '@rollup/plugin-sucrase';
+import config from './tsconfig.json'
 
 const isProduction = !process.env.IS_DEVELOPMENT;
 const sourcemap = !isProduction;
-const clearScreen = { watch: {
-  clearScreen: false,
-  chokidar: true
-} };
+const clearScreen = { watch: { clearScreen: false } };
 
 console.log({ isProduction, sourcemap });
 
-const external = ['react', 'react-dom', 'next/navigation', 'react-router-dom']
+const external = ['react', 'react-dom', 'next/navigation', 'react-router', 'react-router-dom', '@remix-run']
 
 const plugins = [
   resolve({
     include: ['node_modules/**'],
-    extensions: ['.js', '.ts', '.jsx', '.tsx']
   }),
-  sucrase({
-    exclude: ['node_modules/**'],
-    transforms: ['typescript']
+  typescript({
+    tsconfig: './tsconfig.build.json',
+    compilerOptions: { ...config.compilerOptions, sourceMap: sourcemap, declarationMap: sourcemap, declaration: true },
   }),
+
 
   !isProduction && sourcemaps(),
   isProduction && terser({ ecma: '2022' }),
-  // TODO: show total size of JS files
-  isProduction && filesize({ showMinifiedSize: false }),
+  filesize(),
 ].filter(Boolean);
 
 export default [
