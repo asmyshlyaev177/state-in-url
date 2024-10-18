ad5a80145a04156383340872c7e8912c839e0cc6
asmyshlyaev177 <asmyshlyaev177+github@gmail.com>
2024-10-18 13:24:43 +0400

    chore: disable husky for ci

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,13 @@
-import type { Config } from 'jest';
+import type { Config } from "jest";
 
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-const config: Config ={
-  testRegex: '(\\.|/)(test)\\.[jt]sx?$',
-  preset: 'ts-jest',
-  testEnvironment: './test.env.ts',
-  "collectCoverage": true,
-  "coverageReporters": ["json", "html", "lcov"],
-  coverageDirectory: './coverage-reports'
+const config: Config = {
+  testRegex: "(\\.|/)(test)\\.[jt]sx?$",
+  preset: "ts-jest",
+  testEnvironment: "./test.env.ts",
+  collectCoverage: true,
+  coverageReporters: ["json", "html", "lcov"],
+  coverageDirectory: "./coverage-reports",
 };
 
-export default config
+export default config;
