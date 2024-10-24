9f5d7a1a7552d98059b4102af1135378a1b4afca
asmyshlyaev177 <asmyshlyaev177+github@gmail.com>
2024-10-24 22:41:22 +0400

    chore: google tag

--- packages/example-nextjs14/src/app/layout.tsx
+++ packages/example-nextjs14/src/app/layout.tsx
@@ -22,6 +22,7 @@ export default async function RootLayout({
       {/* TODO: https://github.com/vercel/next.js/discussions/53540 */}
       <link rel="canonical" href={vercelUrl}></link>
       <link rel="alternate" href={netifyUrl}></link>
+      <meta name="google-site-verification" content="n2td2kbV0H7_y4De1DI04ZQlo_yoBJrOn9gOw-8Fi5E" />
       <body className={`${roboto.className}`}>
         {children}
       </body>
