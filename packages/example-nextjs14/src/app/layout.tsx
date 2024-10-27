2978d82bee9a3cb063972adfd7e8809a509bca73
asmyshlyaev177 <asmyshlyaev177+github@gmail.com>
2024-10-28 02:08:11 +0400

    fix: minor fix

--- packages/example-nextjs14/src/app/layout.tsx
+++ packages/example-nextjs14/src/app/layout.tsx
@@ -19,7 +19,6 @@ export default async function RootLayout({
     <html lang="en">
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-      {/* TODO: https://github.com/vercel/next.js/discussions/53540 */}
       {isVercel && <link rel="canonical" href={vercelUrl}></link>}
       <meta name="google-site-verification" content="n2td2kbV0H7_y4De1DI04ZQlo_yoBJrOn9gOw-8Fi5E" />
       <body className={`${roboto.className}`}>
