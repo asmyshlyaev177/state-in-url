f36dcf2daf21f0b17e2eba126f40e64c93be6bfd
asmyshlyaev177 <asmyshlyaev177+github@gmail.com>
2024-11-02 05:48:07 +0400

    ci: use Playwright reports

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   // workers: process.env.CI ? 1 : undefined,
   workers: process.env.CI ? 1 : 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'line',
+  reporter: process.env.CI ? 'blob' : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
