9ad5dd993441e92bde0f2da596eb1e20e4f703ca
asmyshlyaev177 <asmyshlyaev177+github@gmail.com>
2024-11-21 17:25:48 +0400

    chore: react-scan for dev

--- packages/shared/components/Refresh.tsx
+++ packages/shared/components/Refresh.tsx
@@ -1,19 +1,22 @@
 import { clsx } from "clsx";
+import React from "react";
 
 import { Button } from "./Button";
-export const RefreshButton = ({ className }: { className?: string }) => {
-  return (
-    <Button
-      onClick={() => {
-        window.location.reload();
-      }}
-      className={clsx(
-        `transition font-extrabold bg-orange-700 text-lg text-white`,
-        className,
-      )}
-      name="Reload page"
-    >
-      Reload page
-    </Button>
-  );
-};
+export const RefreshButton = React.memo(
+  ({ className }: { className?: string }) => {
+    return (
+      <Button
+        onClick={() => {
+          window.location.reload();
+        }}
+        className={clsx(
+          `transition font-extrabold bg-orange-700 text-lg text-white`,
+          className,
+        )}
+        name="Reload page"
+      >
+        Reload page
+      </Button>
+    );
+  },
+);
