f9344451eb581b81aa589cf778f41c6660140689
asmyshlyaev177 <asmyshlyaev177@gmail.com>
2025-09-08 13:49:21 +0400

    style: update metadata

--- packages/example-nextjs14/src/app/seoStuff.ts
+++ packages/example-nextjs14/src/app/seoStuff.ts
@@ -3,6 +3,7 @@ import { type SoftwareApplication, type WithContext } from 'schema-dts';
 import packageJson from '../../../../package.json';
 import { vercelUrl } from './domain';
 import { type Metadata } from 'next';
+import { isProd } from 'consts';
 
 export const jsonLd: WithContext<SoftwareApplication> = {
   '@context': 'https://schema.org',
@@ -23,7 +24,7 @@ export const jsonLd: WithContext<SoftwareApplication> = {
   applicationCategory: 'DeveloperApplication',
   downloadUrl: 'https://github.com/asmyshlyaev177/state-in-url',
   installUrl: 'https://www.npmjs.com/package/state-in-url',
-  url: 'https://state-in-url-asmyshlyaev177.vercel.app',
+  url: 'https://state-in-url.dev',
   sameAs: 'https://state-in-url.netlify.app',
   softwareRequirements: ['JavaScript', 'Web Browser', 'npm'],
   license:
@@ -50,6 +51,9 @@ const meta = {
 };
 
 export const metadata = {
+  metadataBase: isProd
+    ? new URL(vercelUrl)
+    : new URL(`http://localhost:${process.env.PORT || 3000}`),
   title: meta.title,
   description: meta.description,
   icons: {
@@ -60,14 +64,21 @@ export const metadata = {
       },
     ],
   },
+
   openGraph: {
     url: vercelUrl,
-    siteName: 'state-in-url',
+    siteName: 'state-in-url.dev',
     title: meta.title,
     description: meta.description,
     images: [
       {
-        url: `${vercelUrl}/images/logo.svg`,
+        url: `/social_banner.png`,
+        width: 1280,
+        height: 640,
+        alt: 'state-in-url social banner',
+      },
+      {
+        url: `/images/logo.svg`,
         width: 1280,
         height: 640,
         alt: 'state-in-url library image',
@@ -75,5 +86,22 @@ export const metadata = {
     ],
     locale: 'en_US',
     type: 'website',
+  },
+
+  twitter: {
+    card: 'summary_large_image',
+    site: 'state-in-url.dev',
+    creator: '@asmyshlyaev177',
+    title: meta.title,
+    description: meta.description,
+    images: [`/social_banner.png`],
+  },
+
+  alternates: {
+    canonical: vercelUrl,
+  },
+
+  other: {
+    'script:ld+json': JSON.stringify(jsonLd),
   },
 } as Metadata;
