f276d20998dad16b16313a8ef336f1ac5ab630af
asmyshlyaev177 <asmyshlyaev177@gmail.com>
2026-02-12 22:08:19 +0400

    ci: fix build error

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,23 +1,23 @@
-import AutoImport from "unplugin-auto-import/vite";
-import { defineConfig } from "vitest/config";
+import AutoImport from 'unplugin-auto-import/vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    globalSetup: "./setup-tests.ts",
-    environment: "happy-dom",
-    include: ["**/*.test.?(c|m)[jt]s?(x)"],
+    globalSetup: './setup-tests.ts',
+    environment: 'happy-dom',
+    include: ['**/*.test.?(c|m)[jt]s?(x)'],
     coverage: {
       enabled: true,
-      include: ["packages/urlstate"],
-      provider: "istanbul",
-      reportsDirectory: "./coverage-reports",
-      reporter: ["lcov", "json", "html"],
+      include: ['packages/urlstate'],
+      provider: 'istanbul',
+      reportsDirectory: './coverage-reports',
+      reporter: ['lcov', 'json', 'html'],
     },
     globals: true,
   },
   plugins: [
     AutoImport({
-      imports: ["vitest"],
+      imports: ['vitest'],
       dts: true, // generate TypeScript declaration
     }),
   ],
